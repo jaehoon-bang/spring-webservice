3ff9eff293ca7c797ba180f3a03eb93237b498b6
방재훈 <shake0824@gmail.com>
2019-10-28 15:59:20 +0900

    +테스트 커밋

--- src/test/java/com/devjhbang/springwebservice/web/WebRestControllerTest.java
+++ src/test/java/com/devjhbang/springwebservice/web/WebRestControllerTest.java
@@ -9,13 +9,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class WebRestControllerTest
-{
+public class WebRestControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
     @Test
-    public void Profile확인 () {
+    public void Profile확인() {
         //when
         String profile = this.restTemplate.getForObject("/profile", String.class);
 
